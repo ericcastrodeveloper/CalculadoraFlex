80876f67c5fd1215453c69912af3fa3b5c08cd37
Logon Aluno <logonaluno@local.com>
2019-12-14 11:20:02 -0300

    initial commit

---
+++ app/src/main/java/br/com/eric/calculadoraflex/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package br.com.eric.calculadoraflex.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.eric.calculadoraflex.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
